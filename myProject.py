c7099abb195a94b031369e57d0405a4243f956f6
AsaLap <asalaporte@gmail.com>
2018-12-05 10:24:31 +0100

    Les erreurs de Charlotte

--- myProject.py
+++ myProject.py
@@ -16,10 +16,10 @@ def oneWord(seq,pos,wlen): #return a piece with a lenght = wlen of the string se
 
         argument: wlen: longueur de la liste voulue
         pos: position voulue de départ
-        seq:correspond a la séquence Fasta récuperer
+        seq:correspond a la séquence Fasta récupérer
 
-        Return: la fonction return une liste appelée sequence contenant
-        la séquence nucléotidique débutant a la position strat souhaité et d'une longueur wlen
+        Return: la fonction retourne une liste appelée séquence contenant
+        la séquence nucléotidique débutant à la position strat souhaitée et d'une longueur wlen
     '''
     a=0
     sequence=''
@@ -33,11 +33,11 @@ def oneWord(seq,pos,wlen): #return a piece with a lenght = wlen of the string se
 
 def isCodonStart(seq,pos): #return True if the string seq has start codon
     '''
-    La fonction sert à identifier le codon start de la séquence, en parcourant la séquence de 1 en 1 et stocke dans la variable
+    La fonction sert à identifier le codon start de la séquence, en parcourant la séquence de 1 en 1 et stock dans la variable
         codon une suite de 3 nucléotides.
 
         argument: seq:séquence Fasta
-                pos: position de départ du codon de 3 nucléotides a tester
+                pos: position de départ du codon de 3 nucléotides à tester
 
         return: La fonction retourne True si en parcourant la séquence,
         elle trouve un des codons start de la liste, sinon elle retourne False
@@ -51,14 +51,14 @@ def isCodonStart(seq,pos): #return True if the string seq has start codon
 
 def isCodonStop(seq,pos): #return True if seq has a stop codon
     '''
-    la fonction sert à identifier le codon stop de la séquence, en parcourant la séquence de 1 en 1 et stocke
+    la fonction sert à identifier le codon stop de la séquence, en parcourant la séquence de 1 en 1 et stock
             dans la variable codon une suite de 3 nucléotides
 
         argument: seq: Séquence fasta
                 pos:position de départ du codon de 3 nucléotides à tester
 
-        return: La fonction return True, si en parcourant la séquence
-        elle trouve un des deux codon Stop, sinon elle retourne False
+        return: La fonction retourne True, si en parcourant la séquence
+        elle trouve un des deux codons Stop, sinon elle retourne False
     '''
 
     codon=oneWord(seq,pos,3)
@@ -72,10 +72,10 @@ def isGene3(seq,version,threshold=90):
     '''
     La fonction parcours la séquence sur sa longueur -2. Elle cherche alors en appellant la fonction isCodonStart
         si il y a présence d'un codon sans cadre de lecture spécifique. Si elle identifie un codon alors
-        elle parcours de 3 en 3 la séquence en commencant à la position i+threshold, qui correspond a la position
+        elle parcourt de 3 en 3 la séquence en commencant à la position i+threshold, qui correspond a la position
         du codon start plus un seuil minimal d'ORF,par défaut cette valeur de threshold est de 90. Une fois cette distance atteinte,
-        la fonction va alors appelé isCodonStop afin d'identifier un codon stop dans l'ORF associé au codon start identifié, elle s'arrete au premier des qu'elle en trouve un.
-        Une fois un ORF determine, la fonction va print la version de la séquence étudié (5' 3' ou complémentaire inversé),
+        la fonction va alors appeler isCodonStop afin d'identifier un codon stop dans l'ORF associé au codon start identifié, elle s'arrète au premier dès qu'elle en trouve un.
+        Une fois un ORF déterminé, la fonction va print la version de la séquence étudiée (5' 3' ou complémentaire inversé),
         le cadre de lecture (0,1,2), la longueur j+2-i égale au début du codon start et à la fin du codon stop. De plus, elle print la suite nucléotidique du codon start et du codon stop
         et leur position, sinon elle retourne False
 
@@ -83,7 +83,7 @@ def isGene3(seq,version,threshold=90):
                     version: la version peut être 5'3' ou complementaire inversé
                     threshold: cela correspond à une longueur d'ORF minimal afin de limiter les faux positifs
 
-        return: La fonction ne return rien, mais elle print des informations
+        return: La fonction ne retourne rien, mais elle print des informations
     '''
     for i in range(len(seq)-2):
         if isCodonStart(seq,i) == True:
@@ -106,12 +106,12 @@ def isGene3(seq,version,threshold=90):
 #############################
 
 def openFasta(file):
-    '''La fonction a pour objectif de récuperer le fichier Fasta présent dans le repoertoire
-    courant, elle le converti en string, en enlevant les sauts de lignes et le stocke dans une variable
+    '''La fonction à pour objectif de récuperer le fichier Fasta présent dans le repertoire
+    courant, elle le convertit en string, en enlevant les sauts de lignes et le stocke dans une variable
 
     argument: file: nom du fichier source
 
-    return: La fonction return data, c'est la variable qui contient la séquence Fasta standardisé pour le programme.
+    return: La fonction retourne data, c'est la variable qui contient la séquence Fasta standardisée pour le programme.
     '''
     with open(file) as fasta:
         data = fasta.read()
@@ -138,8 +138,8 @@ def four_lectures(seq):
 
 
 def writeFasta(data,FICHIER):
-    '''Cette fonction n'as pas d'utilité a proprement parlé dans l'éxécution du programme
-    elle sert à ecrire dans un fichier txt, les séquences obtenu à la fin de l'éxécution de four-lectures'''
+    '''Cette fonction n'as pas d'utilité à proprement parler dans l'éxécution du programme
+    elle sert à écrire dans un fichier txt, les séquences obtenues à la fin de l'éxécution de four-lectures'''
     fic=open(FICHIER, "w")
     for letter in data :
         fic.write(letter)
